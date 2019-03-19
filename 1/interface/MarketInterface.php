b4ed5af8d7e0fc7862aca670db6db3ae4f447e21
huangwei <huangwei@taqu.cn>
2019-03-19 10:30:25 +0800

    短信注册

--- 1/interface/MarketInterface.php
+++ 1/interface/MarketInterface.php
@@ -1,6 +1,12 @@
 <?php
 class MarketInterface extends BaseInterface
 {
+    public function __construct()
+    {
+        parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+    }
+
     public function getGoodsList()
     {
         $page = getParam('page');
@@ -13,6 +19,46 @@ class MarketInterface extends BaseInterface
         }
 
         $this->respondSuccess($data, array('image_domain' => c('image_domain')));
+    }
+
+    public function sendCode()
+    {
+        $mobile = getParam('mobile');
+
+        $service      = s('Market');
+        $service->sendCode($mobile);
+
+        if ($service->hasError()) {
+            $this->respondFailure($service->getError());
+        }
+
+        $this->respondSuccess(null);
+    }
+
+    public function regMobile()
+    {
+        $mobile = getParam('mobile');
+        $code = getParam('code');
+        $channel_id = getParam('channel_id', 0);
+
+        $service      = s('Market');
+        $service-> regMobile($mobile, $code, $channel_id);
+
+        if ($service->hasError()) {
+            $this->respondFailure($service->getError());
+        }
+
+        $this->respondSuccess(null);
+    }
+
+    public function getAccessStatus()
+    {
+        $data = array(
+            'status' => 0
+        );
+
+        $this->respondSuccess($data);
+
     }
 
 }
--- 1/service/MarketService.php
+++ 1/service/MarketService.php
@@ -26,4 +26,73 @@ class MarketService extends BaseService
         return $list;
     }
 
+    /**
+     * 发送短信验证码
+     */
+    public function sendCode($mobile)
+    {
+        $rand = mt_rand(100000, 999999);
+        $redis = r('psn_redis');
+        $reg_code_key = redis_key('loan_reg_code', $mobile);
+        $redis->set($reg_code_key, $rand, 300);
+
+        $content = "操作验证码: {$rand}, 5分钟内有效。";
+
+        $service = s('ChuanglanSms');
+        $result = $service->sendSMS($mobile, $content);
+        return null;
+
+        if(!is_null(json_decode($result))){
+
+            $output=json_decode($result,true);
+
+            if(isset($output['code'])  && $output['code']=='0'){
+                return null;
+            }else{
+                return $this->setError('send_sms_fail', $output['errorMsg']);
+            }
+        }else{
+            log::e('send_sms_error' . json_encode($result));
+            return $this->setError('unknow_error');
+        }
+    }
+
+    public function regMobile($mobile, $code, $channel_id)
+    {
+        if (empty($mobile)) {
+            return $this->setError('param_mobile_is_empty', '手机号不能为空');
+        }
+
+        if (empty($code)) {
+            return $this->setError('param_code_is_empty', '验证码不能为空');
+        }
+
+
+        $redis = r('psn_redis');
+        $reg_code_key = redis_key('loan_reg_code', $mobile);
+        $check_code = $redis->get($reg_code_key);
+//        if ($code != $check_code) {
+//            return $this->setError('code_is_invalid', '验证码错误或已失效');
+//        } else {
+//            $redis->expire($reg_code_key, 0);
+//        }
+
+        $db = pdo('loan_db');
+        $db->tableName = 'account';
+
+        $where['mobile'] = $mobile;
+        $info = $db->find($where);
+
+        if (!empty($info)) {
+            return $this->setError('mobile_already_reg', '该手机号已注册过');
+        }
+
+        $data = array(
+            'mobile' => $mobile,
+            'channel_id' => $channel_id,
+            'create_time' => time(),
+        );
+
+        $db->insert($data);
+    }
 }
