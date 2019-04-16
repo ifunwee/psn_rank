c6fc114259f23bf0a7e60c79fdd8f21e7fdd98fe
huangwei <huangwei@taqu.cn>
2019-04-16 16:25:51 +0800

    跨域优化

--- 1/interface/MarketInterface.php
+++ 1/interface/MarketInterface.php
@@ -4,7 +4,14 @@ class MarketInterface extends BaseInterface
     public function __construct()
     {
         parent::__construct();
-        header('Access-Control-Allow-Origin: *');
+        if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+            header('Access-Control-Allow-Methods: *');
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 3600');
+            exit;
+        }
     }
 
     public function getGoodsList()
