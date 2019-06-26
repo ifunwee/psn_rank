0ac234c51a52927d34d251426fa3b398bc600c73
huangwei <huangwei@taqu.cn>
2019-06-27 00:45:11 +0800

    图片上传新增avatar

--- 1/service/ToolsService.php
+++ 1/service/ToolsService.php
@@ -4,7 +4,7 @@ class ToolsService extends BaseService
     public $max_size = 10000000; //10M
     public $mimes = array();
     public $exts = array('gif', 'jpg', 'jpeg', 'bmp', 'png');
-    public $businiess_type = array('comment', 'banner');
+    public $businiess_type = array('comment', 'banner', 'avatar');
 
     public function upload($files, $business_type, $bucket = 'app-image')
     {
