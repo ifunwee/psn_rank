413a444abd0c9d19f6c96240bbda16206a6dd6fe
huangwei <huangwei@taqu.cn>
2019-09-23 01:13:22 +0800

    增加jwt支持

--- 1/interface/UserInterface.php
+++ 1/interface/UserInterface.php
@@ -3,7 +3,7 @@ class UserInterface extends BaseInterface
 {
     public function buildJWT()
     {
-        $service = s('User');
+        $service = s('Common');
         $payload = array('sub'=>'1234567890','name'=>'John Doe','iat'=>1516239022);
 
         $result = $service->buildJWT($payload);
@@ -17,7 +17,7 @@ class UserInterface extends BaseInterface
     public function parseJWT()
     {
         $token = getParam('token');
-        $service = s('User');
+        $service = s('Common');
         $result = $service->parseJWT($token);
         if ($service->hasError()) {
             $this->respondFailure($service->getError());
